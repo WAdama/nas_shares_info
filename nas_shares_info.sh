a81f4286da8a5137bc4165614397aac8940ad963
WAdama <push@asche-rz.de>
2019-05-11 18:13:46 +0200

    Create nas_shares_info.sh

---
+++ nas_shares_info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CONF=$1
+TOTAL=0
+source $CONF
+echo "<prtg><?xml version=\"10.0\" encoding=\"UTF-8\" ?>"
+for SHARE in "${SHARES[@]}"
+do
+REC="/$VOLUME/$SHARE/""#recycle/"
+IFS=" " read USED VOLNAME <<< `du -s /$VOLUME/$SHARE/`
+TOTAL=$((TOTAL + USED))
+echo "<result><channel>Volume $SHARE: In Use</channel><value>`echo | gawk -v PRE=${USED} 'BEGIN {OFMT="%.2f";print PRE / 1048576}'`</value><unit>Custom</unit><CustomUnit>GByte</CustomUnit><float>1</float></result>"
+if [ -d $REC ]
+  then
+	IFS=" " read RECY RECNAME <<< `du -s $REC`
+	echo "<result><channel>Volume $SHARE: Recyclable</channel><value>`echo | gawk -v PRE=${RECY} 'BEGIN {OFMT="%.2f";print PRE / 1048576}'`</value><unit>Custom</unit><CustomUnit>GByte</CustomUnit><float>1</float></result>"
+fi
+done
+echo "<result><channel>Total</channel><value>`echo | gawk -v PRE=${TOTAL} 'BEGIN {OFMT="%.2f";print PRE / 1048576}'`</value><unit>Custom</unit><CustomUnit>GByte</CustomUnit><float>1</float></result></prtg>"
+exit
