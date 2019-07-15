8887db40dd7714fcb95bebef3f33045c7eaeae13
Ingo <foren@asche-rz.de>
2019-07-15 13:09:51 +0200

    Now uses PRTG standard units

--- nas_shares_info.sh
+++ nas_shares_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.0
+# Version 1.0.1
 #Be sure you have valid SSH credentials in your device
 
 CONF=$1
@@ -9,14 +9,15 @@ echo "<prtg><?xml version=\"10.0\" encoding=\"UTF-8\" ?>"
 for SHARE in "${SHARES[@]}"
 do
 REC="/$VOLUME/$SHARE/""#recycle/"
+SNAP="/$VOLUME/@sharesnap/$SHARE"
 IFS=" " read USED VOLNAME <<< `du -s /$VOLUME/$SHARE/`
 TOTAL=$((TOTAL + USED))
-echo "<result><channel>Volume $SHARE: In Use</channel><value>`echo | gawk -v PRE=${USED} 'BEGIN {OFMT="%.2f";print PRE / 1048576}'`</value><unit>Custom</unit><CustomUnit>GByte</CustomUnit><float>1</float></result>"
+echo "<result><channel>Volume $SHARE: In Use</channel><value>$((USED*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
 if [ -d $REC ]
   then
 	IFS=" " read RECY RECNAME <<< `du -s $REC`
-	echo "<result><channel>Volume $SHARE: Recyclable</channel><value>`echo | gawk -v PRE=${RECY} 'BEGIN {OFMT="%.2f";print PRE / 1048576}'`</value><unit>Custom</unit><CustomUnit>GByte</CustomUnit><float>1</float></result>"
+	echo "<result><channel>Volume $SHARE: Recyclable</channel><value>$((RECY*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
 fi
 done
-echo "<result><channel>Total</channel><value>`echo | gawk -v PRE=${TOTAL} 'BEGIN {OFMT="%.2f";print PRE / 1048576}'`</value><unit>Custom</unit><CustomUnit>GByte</CustomUnit><float>1</float></result></prtg>"
+echo "<result><channel>Total</channel><value>$((TOTAL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result></prtg>"
 exit
