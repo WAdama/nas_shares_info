b8a967718b6ecd99d1669c7f074720cc3ab16a19
Ingo <foren@asche-rz.de>
2019-07-18 15:04:36 +0200

    Monir correction

--- nas_shares_info.sh
+++ nas_shares_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.0.1
+# Version 1.0.2
 #Be sure you have valid SSH credentials in your device
 
 CONF=$1
@@ -10,13 +10,13 @@ for SHARE in "${SHARES[@]}"
 do
 REC="/$VOLUME/$SHARE/""#recycle/"
 SNAP="/$VOLUME/@sharesnap/$SHARE"
-IFS=" " read USED VOLNAME <<< `du -s /$VOLUME/$SHARE/`
+IFS=" " read USED VOLNAME <<< `du -s -b /$VOLUME/$SHARE/`
 TOTAL=$((TOTAL + USED))
-echo "<result><channel>Volume $SHARE: In Use</channel><value>$((USED*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
+echo "<result><channel>Volume $SHARE: In Use</channel><value>$USED</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
 if [ -d $REC ]
   then
-	IFS=" " read RECY RECNAME <<< `du -s $REC`
-	echo "<result><channel>Volume $SHARE: Recyclable</channel><value>$((RECY*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
+	IFS=" " read RECY RECNAME <<< `du -s -b $REC`
+	echo "<result><channel>Volume $SHARE: Recyclable</channel><value>$RECY</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
 fi
 done
 echo "<result><channel>Total</channel><value>$((TOTAL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result></prtg>"
