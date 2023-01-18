bd3149e0ce778b1329c550e8943a1587cd58d941
Ingo <foren@asche-rz.de>
2023-01-18 15:41:57 +0100

    Optimized code with ShellCheck

--- nas_shares_info.sh
+++ nas_shares_info.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-# Version 1.0.4
-#Be sure you have valid SSH credentials in your device
+# Version 1.0.5
 
-CONF=$1
 TOTAL=0
-source $CONF
+source $1
 echo "<prtg><?xml version=\"10.0\" encoding=\"UTF-8\" ?>"
 for SHARE in "${SHARES[@]}"
 do
-REC="/$VOLUME/$SHARE/""#recycle/"
-SNAP="/$VOLUME/@sharesnap/$SHARE"
-IFS=" " read USED VOLNAME <<< $(du -s -b /$VOLUME/$SHARE/)
-TOTAL=$((TOTAL + USED))
-echo "<result><channel>Share $SHARE: In Use</channel><value>$USED</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
-if [ -d $REC ]
+REC="/$VOLUME/$SHARE/#recycle/"
+IFS=$'\t' read -r "USED[0]" "USED[1]" <<< "$(du -s -b /"$VOLUME"/"$SHARE"/)"
+TOTAL=$((TOTAL + USED[0]))
+echo "<result><channel>Share $SHARE: In Use</channel><value>${USED[0]}</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
+if [ -d "$REC" ]
   then
-	IFS=" " read RECY RECNAME <<< $(du -s -b $REC)
-	echo "<result><channel>Share $SHARE: Recyclable</channel><value>$RECY</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
+	IFS=$'\t' read -r "RECY[0]" "RECY[1]" <<< "$(du -s -b "$REC")"
+	echo "<result><channel>Share $SHARE: Recyclable</channel><value>${RECY[0]}</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>"
 fi
 done
 echo "<result><channel>Total</channel><value>$((TOTAL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result></prtg>"
 exit
-
